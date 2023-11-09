2234c84b975f570af13409eeed2975e51dec7f78
Eden <ecoding@pm.me>
2023-11-09 01:51:44 -0800

    Initial Commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+
+namespace main{
+    class Program{
+        static IEnumerable<string> EnumerateFilesRecursively(string path){
+
+        }
+        
+        static string FormatByteSize(long byteSize){
+        
+        }
+        
+        static XDocument CreateReport(IEnumerable<string> files){
+        
+        }
+        
+        public static void Main(string[] args){
+        
+        }
+    }
+}
+
