bb78b33c1cb367e78e3bc59cc08ff25977e12ebd
Kyla Do <117338981+kyoonies@users.noreply.github.com>
2023-11-11 14:03:51 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.IO.FileInfo;
 using System.Xml.Linq.XElement;
@@ -7,7 +8,18 @@ using System.IO.Directory;
 namespace main{
     class Program{
         static IEnumerable<string> EnumerateFilesRecursively(string path){
+            // iterate over each file in the path and yield it
+            foreach (var file in Directory.GetFiles(path)){
+                yield return file;
+            }
 
+            // iterate over each subdirectory in the path and enter a recursive call
+            foreach (var subdirectory in Directory.GetDirectories(path)){
+                // each subdirectory enters a recursive call and yield the file
+                foreach (var file in EnumerateFilesRecursively(subdirectory)){
+                    yield return file;
+                }
+            }
         }
         
         static string FormatByteSize(long byteSize){
