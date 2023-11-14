35096e83391c201d823ab503f6fd3726a4973c4b
Kyla Do <117338981+kyoonies@users.noreply.github.com>
2023-11-14 13:18:41 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -7,15 +7,17 @@ using System.Xml.Linq;
 
 namespace main{
     class Program{
-        // used ChatGPT and Microsoft Learn
+        // sources: ChatGPT and Microsoft Learn
         static IEnumerable<string> EnumerateFilesRecursively(string path){
             // iterate over each file in the path and yield it
-            foreach (var file in Directory.GetFiles(path)){
-                yield return file;
+            var fileEntries = Directory.GetFiles(path);
+            foreach (var fileName in fileEntries){
+                yield return fileName;
             }
 
             // iterate over each subdirectory in the path and enter a recursive call
-            foreach (var subdirectory in Directory.GetDirectories(path)){
+            var subdirectoryEntries = Directory.GetDirectories(path);
+            foreach (var subdirectory in subdirectoryEntries){
                 // each subdirectory enters a recursive call and yield the file
                 foreach (var file in EnumerateFilesRecursively(subdirectory)){
                     yield return file;
